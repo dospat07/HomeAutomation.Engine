312a0f2c2bef4a9639da6136b422bec905861fc4
dospat07 <dospat07@gmail.com>
2017-05-31 12:46:18 +0300

    Microsoft.NETCore.App Version="2.1.0-preview1-25329-02"

--- Persistance/HomeAutomationContext.cs
+++ Persistance/HomeAutomationContext.cs
@@ -14,7 +14,7 @@ namespace HomeAutomation.Engine.Persistance
         public DbSet<User> Users { get; set; }
         public DbSet<Temperature> Temperatures { get; set; }
         public DbSet<HourlyTemperature> HourlyTemperatures { get; set; }
-
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Filename=data.db");
@@ -26,8 +26,7 @@ namespace HomeAutomation.Engine.Persistance
 
             var insertSql = $"insert into hourlytemperatures (roomID,date,value) select roomID, strftime('%Y-%m-%d %H:00:00', date) as Date, sum(value) / count(*) as Value from temperatures where date<'{now}' group by roomID, strftime('%Y-%m-%d %H', date)";
             var deleteSql = $"delete from temperatures where date<'{now}'";
-            Console.WriteLine(insertSql);
-            Console.WriteLine(deleteSql);
+           
             this.Database.ExecuteSqlCommand(insertSql);
             this.Database.ExecuteSqlCommand(deleteSql);
             this.SaveChanges();
